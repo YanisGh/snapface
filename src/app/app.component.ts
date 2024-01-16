ce882287277f5570c73d751a0f32f1ae94a9b0a1
John Doe <yanisghoul35@gmail.com>
2024-01-16 11:00:07 +0100

    Observables opérateurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable, map, filter, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  interval$!: Observable<string>;
+
   ngOnInit() {
-    
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+          `Je suis ${value} et je suis pair` :
+          `Je suis ${value} et je suis impair`
+      ),
+      tap(text => this.logger(text))
+    );
   }
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
+}
 }
 
